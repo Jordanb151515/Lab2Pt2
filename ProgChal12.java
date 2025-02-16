77ba6cc912f05ee5cadf7e21475a4d11800cba22
Jordan Byrne <jbyrne001@student.sdccd.edu>
2025-02-16 10:47:24 -0800

    updated chall 12

--- ProgChal12.java
+++ ProgChal12.java
@@ -11,6 +11,25 @@ public class ProgChal12
 {
     public static void main(String[] args){
         String city;
+        String cityUpper, cityLower;
+        char cityFirst;
+        int cityChars;
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Enter your favorite city: ");
+        city = keyboard.nextLine();
+        cityChars = city.length();
+        cityUpper = city.toUpperCase();
+        cityLower = city.toLowerCase();
+        cityFirst = city.charAt(0);
+        
+        System.out.println("Your favorite city has: " + cityChars + " characters");
+        System.out.println("In upper case: " + cityUpper);
+        System.out.println("In lower case: " + cityLower);
+        System.out.println("The first character is: " + cityFirst);
+        
+        
         
     }
 }
